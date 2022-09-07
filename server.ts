a7f1f44601a1da918a8adcc5731190e57b13d341
meenal17 <76942400+meenal17@users.noreply.github.com>
2022-09-07 13:31:54 +0530

    main chnges

--- server.ts
+++ server.ts
@@ -168,6 +168,6 @@ app.get('*', async (req: any, res: any) => {
     res.sendFile(path.join(__dirname, "client", "index.html"))
 })
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server started at port ${process.env.PORT || 5000}`)
+app.listen(process.env.PORT || 5500, () => {
+    console.log(`Server started at port ${process.env.PORT || 5500}`)
 })
